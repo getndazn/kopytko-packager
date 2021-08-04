b5c6c9ff07ef57cba1139d60d7d39201f09a0197
Paweł Hertman <pawel@hertman.eu>
2021-08-04 14:05:31 +0200

    feat: Optional environments config field + basic example app

--- src/env/validate-args.js
+++ src/env/validate-args.js
@@ -2,7 +2,7 @@ const term = require('terminal-kit').terminal;
 
 const args = require('./args');
 
-module.exports = async function validateArgs() {
+module.exports = async function validateArgs(buildOnlyMode = false) {
   if (args.forceHttp) {
     term.red('DANGER!!!! You are going to use unsecured http protocol for every URL in the manifest.\nAre you sure you want to continue? (y/n)\n');
     const result = await term.yesOrNo({ yes: ['y'], no: ['n', 'ENTER', 'ESCAPE'] }).promise;
@@ -12,7 +12,9 @@ module.exports = async function validateArgs() {
     term.grabInput(false);
   }
 
-  if (!args.rokuIP) throw new Error('Configure ROKU_IP in the .env file or pass as an environment variable!');
+  if (!buildOnlyMode && !args.rokuIP) {
+    throw new Error('Configure ROKU_IP in the .env file or pass as an environment variable!');
+  }
 
   return args;
 }

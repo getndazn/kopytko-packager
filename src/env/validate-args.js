e93fd5656601e510b8a7567c2b35d2f735cf264d
Paweł Hertman <pawel.hertman@dazn.com>
2021-08-05 14:57:56 +0200

    feat: Optional environments config field + basic example app (#12)
    * feat: Optional environments config field + basic example app
    
    * CR-FIX
    
    * CR-FIX
    
    * Changed interface of validateArgs

--- src/env/validate-args.js
+++ src/env/validate-args.js
@@ -2,7 +2,12 @@ const term = require('terminal-kit').terminal;
 
 const args = require('./args');
 
-module.exports = async function validateArgs() {
+/**
+ * @param {Object} [options={}]
+ * @param {boolean} options.buildOnlyMode
+ * @returns {Promise<Object>}
+ */
+module.exports = async function validateArgs(options = {}) {
   if (args.forceHttp) {
     term.red('DANGER!!!! You are going to use unsecured http protocol for every URL in the manifest.\nAre you sure you want to continue? (y/n)\n');
     const result = await term.yesOrNo({ yes: ['y'], no: ['n', 'ENTER', 'ESCAPE'] }).promise;
@@ -12,7 +17,9 @@ module.exports = async function validateArgs() {
     term.grabInput(false);
   }
 
-  if (!args.rokuIP) throw new Error('Configure ROKU_IP in the .env file or pass as an environment variable!');
+  if (!options.buildOnlyMode && !args.rokuIP) {
+    throw new Error('Configure ROKU_IP in the .env file or pass as an environment variable!');
+  }
 
   return args;
 }

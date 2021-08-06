9a06816f019ea525e92260fdbdeb28a78daa3fc5
Paweł Hertman <pawel@hertman.eu>
2021-08-06 15:59:31 +0200

    refactor: Removed unit tests code

--- examples/basic/manifest.js
+++ examples/basic/manifest.js
@@ -4,7 +4,5 @@ module.exports = {
   ...baseManifest,
   title: 'Kopytko Packager: Basic example',
   mm_icon_focus_hd: 'pkg:/images/icon.png',
-  mm_icon_focus_sd: 'pkg:/images/icon.png',
-  splash_screen_hd: 'pkg:/images/icon.png',
-  splash_screen_sd: 'pkg:/images/icon.png',
+  ui_resolutions: 'fhd',
 }
